6c98da6bc25a7e2358d9efedbbc976d8f154cef9
Rmaki27 <engineermaki27@gmail.com>
2023-04-26 22:09:32 -0700

    Add count increment to determine how many times a specific items add button has been clicked

--- data.js
+++ data.js
@@ -1,19 +1,21 @@
 const merchItems = [
     {
         itemName: 'Shirt',
+        data: 'shirt',
         price: 20,
         description: 'Heavyweight garment dyed 100% cotton t-shirt. Screen print on chest and back. Printed label. Standard Fit.',
-        idName: 'shirt'
     },
 
     {
         itemName: 'Vinyl',
+        data: 'vinyl',
         price: 29,
         description: 'Silver vinyl LP with bumper sticker.'
     },
 
     {
         itemName: 'Bucket Hat',
+        data: 'bucket',
         price: 55,
         description: 'Limited capsule collection made in collaboration with Brain Dead for our "2 Days of Music" camping weekend event in Big Sur, California.'
     }
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import { merchItems } from "./data.js"
 
+let countShirt = 0
+let countVinyl = 0
+let countBucketHat = 0
+
 document.addEventListener('click', function (e) {
+
     if (e.target.dataset.shirt) {
-        console.log(e.target.dataset.shirt)
+        countShirt++
+        // console.log(e.target.dataset.shirt)
+        console.log(`shirt count = ${countShirt}`)
+
+    }
+
+    if (e.target.dataset.vinyl) {
+        countVinyl++
+        // console.log(e.target.dataset.vinyl)
+        console.log(`vinyl count = ${countVinyl}`)
+    }
+
+    if (e.target.dataset.bucket) {
+        countBucketHat++
+        // console.log(e.target.dataset.bucket)
+        console.log(`bucket hat count = ${countBucketHat}`)
+
     }
 })
 
@@ -13,14 +34,23 @@ function getItemsListHtml() {
         itemsList += `
         <div class="merch-item">
             <h2>${item.itemName}</h2>
-            <i class="gg-add" id="gg-add" data-${item.itemName}="${item.itemName}"></i>
-            <p>${item.price}</p>
+            <i class="gg-add" id="gg-add" data-${item.data}="${item.data}"></i>
+            <p>$${item.price}</p>
         </div>`
     })
     return itemsList
 }
 
+function getCartHtml() {
+    let cartList = ''
+
+
+
+
+}
+
 document.getElementById('items-list').innerHTML = getItemsListHtml()
+
 
 
 
